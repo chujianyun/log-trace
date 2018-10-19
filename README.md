950622baa1e8ad9766d322be6e23fb7de4b9b91c
sunjinwei <sunjinwei@jd.com>
2018-10-19 11:57:14 +0800

    整理logback日志格式

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # log-trace
 使用logback 记录接口调用traceid 微服务全链路跟踪
+
+
+- 1、添加拦截器，或者切面
+
+```
+参考切面  com.xiao.aop.ServiceLogAspect
+
+参考拦截器 com.xiao.interceptor.LoginInterceptor
+```
+
+- 2、配置 logback 日志添加 traceId 参数
+
+```
+<appender name="console" class="ch.qos.logback.core.ConsoleAppender">
+    <encoder>
+        <pattern>%d{yy-MM-dd.HH:mm:ss.SSS} [%-16t] %-5p %-22c{0}  %X{ServiceId} [%X{traceId}] - %m%n</pattern>
+    </encoder>
+</appender>
+```
--- trace-demo/src/main/java/com/xiao/web/HelloWorldController.java
+++ trace-demo/src/main/java/com/xiao/web/HelloWorldController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by sunjinwei on 2018/1/4.
@@ -15,16 +16,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloWorldController {
 
-
     /**
      * 返回到 jsp 页面
      *
      * @return
      */
     @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    @ResponseBody
     public String welcome() {
         log.info("### welcome:{}", "123");
-        return "init/welcome";
+        return "success";
     }
 
 
